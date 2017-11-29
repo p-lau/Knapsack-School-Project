e207bccb9e752ea9b0ac933854561a8864c95c90
Avorent <34078543+Avorent@users.noreply.github.com>
2017-11-29 04:16:47 -0500

    Add files via upload

--- Project 3/Project 3.py
+++ Project 3/Project 3.py
@@ -8,45 +8,31 @@ from itertools import combinations
 from tkinter import *
 import ast
 
-with open('input.txt', 'r') as f:
-    # read = f.readlines(1)
-    # capacity = read
-    # weight = f.readlines(2)
-    x = f.read().splitlines()
-    capacity = ast.literal_eval(x[0])
-    weight = ast.literal_eval(x[1])
-    value = ast.literal_eval(x[2])
-    ranges = [x for x in range(len(weight)+1)]
-
-    inputSample = tuple(zip(ranges[1:], weight, value))
-    print(inputSample)
-
 sample1 = ((1, 3, 25), (2, 2, 20), (3, 1, 15), (4, 4, 40), (5, 5, 50))
 
 
 def exhaustive(item, capacity):
-    def anycombination(Item):
+    def anycomb(x):
         """ return combinations of any length from the items """
         return (comb
-                for r in range(1, len(Item) + 1)
-                for comb in combinations(Item, r)
+                for r in range(1, len(x) + 1)
+                for comb in combinations(x, r)
                 )
 
     def totalvalue(x):
         """ Sums up a combination of items """
         total_weight = total_value = 0
-        for Item, Weight, Value in x:
-            total_weight += Weight
-            total_value += Value
+        for i, w, v in x:
+            total_weight += w
+            total_value += v
         return (total_value, -total_weight) if total_weight <= capacity else (0, 0)
 
-    result = max(anycombination(item), key=totalvalue)  # max val or min wt if values are equal to each other
-    print("Found items:\n  #" +
-          '\n  #'.join(sorted(str(Item) for Item, _, _ in result)))
+    result = max(anycomb(item), key=totalvalue)  # max val or min wt if values are equal to each other
     val, wt = totalvalue(result)
-    print("for a maximum of %i and a total weight of %i" % (val, -wt))
-
-exhaustive(inputSample, capacity)
+    answer = ('\n\n Finished! The optimal set of items is/are:\n       Item ' +
+              '\n       Item '.join(sorted(str(Item) for Item, _, _ in result)) +
+              '\n for a maximum value of {} and a total weight of {}'.format(val, -wt))
+    return str(answer)
 
 # Step 2. Code a DP method to find the optimal solution to the problem.
 
@@ -60,19 +46,27 @@ class KnapsackUI(Frame):
     def __init__(self, master):
         super().__init__(master)
         self.configure(bg="SkyBlue")
-        self.DialogText = None
-        self.MainMenu()
+        self.DialogText = ""
+        self.menu()
+        self.currentSample = [sample1, 6]
+        with open('input.txt', 'r') as f:
+            x = f.read().splitlines()
+            self.inputCap = ast.literal_eval(x[0])
 
-    def MainMenu(self):
+            ranges = [x for x in range(len(ast.literal_eval(x[1]))+1)]
 
-        self.knapimg = Label(self, image=sackimage, bg="SkyBlue")
+            self.inputSample = tuple(zip(ranges[1:], ast.literal_eval(x[1]), ast.literal_eval(x[2])))
+
+    def menu(self):
+
+        self.knapimg = Label(self, image=sack, bg="SkyBlue")
         self.knapimg.pack(side="top")
 
         self.q = Button(self, text="EXIT", fg="GhostWhite", bg="Crimson", activebackground="GhostWhite", activeforeground="Crimson", command=root.destroy)
         self.h = Button(self, text="HELP", fg="DarkRed", bg="LightCoral", activebackground="DarkRed", activeforeground="LightCoral", command=self.tutorial)
         self.c = Button(self, text="RECURSION", fg="Sienna", bg="Wheat", activebackground="Sienna", activeforeground="Wheat")
         self.DP = Button(self, text="DYNAMIC PROGRAMMING", fg="DarkSlateGray", bg="LightGreen", activebackground="DarkSlateGray", activeforeground="LightGreen")
-        self.ex = Button(self, text="EXHAUSTIVE", fg="DarkSlateGray", bg="MediumSpringGreen", activebackground="DarkSlateGray", activeforeground="MediumSpringGreen", command=self.Exhaustive)
+        self.ex = Button(self, text="EXHAUSTIVE", fg="DarkSlateGray", bg="MediumSpringGreen", activebackground="DarkSlateGray", activeforeground="MediumSpringGreen", command=self.exhaust)
         MainMenuButtons = [self.q, self.h, self.c, self.DP, self.ex]
 
         self.introTitle = Label(self, text="The Knapsack", fg="White", bg="SkyBlue")
@@ -109,49 +103,90 @@ class KnapsackUI(Frame):
         t.exit = Button(t, text="THANKS", fg="GhostWhite", bg="Crimson", activebackground="GhostWhite", activeforeground="Crimson", command=t.destroy)
         self.expandBottom(t.exit)
 
-        self.OneAtATime(t)
+        self.limitwindows(t)
 
-    def Exhaustive(self):
+    def exhaust(self):
         t = Toplevel()
         t.title("Knapsack: Exhaustive Algorithm")
+        t.geometry(f'{int(width/2)}x{int(height/2)}')
         t.resizable(0,0)
         t.config(bg="DarkSlateGrey")
-
-        t.Image = Label(t, image=ex, bg="DarkSlateGrey")
-        t.Image.pack(side="top")
-        t.Title = Label(t, text="Exhaustive", fg="MediumSpringGreen", bg="DarkSlateGrey")
+        x = Text(t, fg="DarkSlateGrey", bg="Ivory")
+        t.Title = Label(t, text="Knapsack: Exhaustive Algorithm", fg="MediumSpringGreen", bg="DarkSlateGrey")
         self.Title(t.Title)
 
-        t.exit = Button(t, text="CLOSE", fg="GhostWhite", bg="Crimson", activebackground="GhostWhite", activeforeground="Crimson", command=t.destroy)
-        self.expandBottom(t.exit)
-        t.enter = Button(t, text="START", fg="DarkSlateGrey", bg="Gold", activebackground="DarkSlateGrey", activeforeground="Gold")
-        self.expandBottom(t.enter)
-        t.load = Button(t, text="LOAD FILE", fg="DarkSlateGrey", bg="MediumSpringGreen", activebackground="DarkSlateGrey", activeforeground="MediumSpringGreen")
-        self.expandBottom(t.load)
+        t.Frame = Frame(t)
+        t.Frame.pack(side="bottom", fill='x', expand=0)
 
-        self.OneAtATime(t)
+        t.Frame.load = Button(t.Frame, text="LOAD FILE", fg="DarkSlateGrey", bg="MediumSpringGreen",
+                              activebackground="DarkSlateGrey", activeforeground="MediumSpringGreen", command=lambda: self.updateSample(x))
+        self.normalButton(t.Frame.load)
+        t.Frame.enter = Button(t.Frame, text="START", fg="DarkSlateGrey", bg="Gold", activebackground="DarkSlateGrey", activeforeground="Gold"
+                               , command=lambda: self.calculateExhaust(x))
+        self.normalButton(t.Frame.enter)
+        t.Frame.exit = Button(t.Frame, text="EXIT", fg="GhostWhite", bg="Crimson", activebackground="GhostWhite", activeforeground="Crimson", command=t.destroy)
+        self.normalButton(t.Frame.exit)
+
+        x.config(font=("Georgia", 12), bd=0, width=100, state="disabled")
+        x.pack(side="bottom", fill='both', expand='1', padx=10, pady=10)
+        self.textintro(x)
+
+        self.limitwindows(t)
+
+    def textintro(self, text):
+        self.DialogText = ""
+        with open('CurrentSample.txt', 'r') as f:
+            self.DialogText = (f.read())
+        text.config(state="normal")
+        text.insert(END, self.DialogText)
+        x = self.currentSample
+        for i in range(len(self.currentSample[0])):
+            text.insert(END, '\n        Item {} has a weight of {} pounds and has a value of {}.'.format(x[0][i][0], x[0][i][1], x[0][i][2]))
+        text.insert(END, '\n\nThe capacity of which how much weight the knapsack can hold is {} pounds.'.format(self.currentSample[1]))
+        text.config(state="disabled")
+
+    def updateSample(self, text):
+        self.currentSample = [self.inputSample, self.inputCap]
+        self.DialogText = ""
+        text.config(state="normal")
+        text.insert(END, '\n\n Load success! The sample has been updated.'
+                         '\n    Here are the new item(s):')
+        x = self.currentSample
+        for i in range(len(self.currentSample[0])):
+            text.insert(END, '\n        Item {} has a weight of {} pounds and has a value of {}.'.format(x[0][i][0], x[0][i][1], x[0][i][2]))
+        text.insert(END, '\n\nThe knapsack can now hold {} pounds.'.format(self.currentSample[1]))
+        text.see(END)
+        text.config(state='disabled')
+
+    def calculateExhaust(self, text):
+        self.DialogText = ""
+        text.config(state="normal")
+        text.insert(END, exhaustive(self.currentSample[0], self.currentSample[1]))
+        text.see(END)
+        text.config(state='disabled')
+
 
     def normalButton(self, Button):
         self.x = Button
-        self.x.config(font=("Helvetica", 16, "bold"), bd=0, width=16)
-        self.x.pack(side="bottom", padx=0, pady=0)
+        self.x.config(font=("Helvetica", 16, "bold"), bd=0)
+        self.x.pack(side="left",fill='x', expand=1, padx=0, pady=0)
 
     def expandBottom(self, Button):
         self.x = Button
         self.x.config(font=("Helvetica", 16, "bold"), bd=0)
-        self.x.pack(side="bottom", fill='both', expand=1, padx=0, pady=0)
+        self.x.pack(side="bottom", fill='x', expand=1, padx=0, pady=0)
 
     def Title(self, Label):
         self.x = Label
         self.x.config(font=("Helvetica", 50, "bold"))
-        self.x.pack(side="top", fill='both', padx=5)
+        self.x.pack(side="top", fill='x', padx=10, pady=10)
 
     def Body(self, Label):
         self.x = Label
         self.x.config(font=("Georgia", 11))
         self.x.pack(side="top", fill='both', expand=0, anchor=W)
 
-    def OneAtATime(self, t):
+    def limitwindows(self, t):
         t.transient(root)
         t.grab_set()
         root.wait_window(t)
@@ -159,8 +194,9 @@ class KnapsackUI(Frame):
 
 root = Tk()
 root.title("The Knapsack")
-sackimage = PhotoImage(file="knapsack1.png")
-ex = PhotoImage(file="fire.png")
+sack = PhotoImage(file="knapsack1.png")
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
 root.resizable(0, 0)
 
 
